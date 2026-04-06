ab544628bc64f8cd645f03005d9bc0caac26fcb9
Thomas Ahn <thomasahn2009@gmail.com>
2026-04-05 23:44:37 -0700

    now getting string instead of table for arg when deploying code

--- lua/wpilib/commands/build.lua
+++ lua/wpilib/commands/build.lua
@@ -1,25 +1,24 @@
 Command = {}
 
 function Command.build(args)
-	if not require('wpilib.commands.commands').is_valid_project() then
+	if not require("wpilib.commands.commands").is_valid_project() then
 		vim.notify("Not a WPILib project", vim.log.levels.ERROR)
 		return
 	end
-	local notifier = require('wpilib.notifier').create_notifier("Starting Robot Build Code")
-	require('wpilib.commands.commands').run_gradlew("build" .. args,
-		function(_, data)
-			if not data or data == "" then return end
-
-			notifier.update("Building Robot Code...", 50)
-
-			if data:find("BUILD FAILED") then
-				notifier.finish("Deploy Failed! Check logs.")
-			end
-		end,
-		function(_)
-			notifier.finish("Robot Code Built!")
+	local notifier = require("wpilib.notifier").create_notifier("Starting Robot Build Code")
+	require("wpilib.commands.commands").run_gradlew("build" .. (args.args or ""), function(_, data)
+		if not data or data == "" then
+			return
 		end
-	)
+
+		notifier.update("Building Robot Code...", 50)
+
+		if data:find("BUILD FAILED") then
+			notifier.finish("Deploy Failed! Check logs.")
+		end
+	end, function(_)
+		notifier.finish("Robot Code Built!")
+	end)
 end
 
 return Command
--- lua/wpilib/commands/deploy.lua
+++ lua/wpilib/commands/deploy.lua
@@ -1,28 +1,27 @@
 Command = {}
 
 function Command.deploy(args)
-	if not require('wpilib.commands.commands').is_valid_project() then
+	if not require("wpilib.commands.commands").is_valid_project() then
 		vim.notify("Not a WPILib project", vim.log.levels.ERROR)
 		return
 	end
 
-	local notifier = require('wpilib.notifier').create_notifier("Deploy Robot Code")
-	require('wpilib.commands.commands').run_gradlew("deploy" .. args,
-		function(_, data)
-			if not data or data == "" then return end
-
-			notifier.update("Building Robot Code...", 50)
-
-			if data:find("BUILD SUCCESSFUL") then
-				notifier.update("Robot Code Built!", 75)
-			elseif data:find("BUILD FAILED") then
-				notifier.finish("Deploy Failed! Check logs.")
-			end
-		end,
-		function(_)
-			notifier.finish("Robot Code Deployed!")
+	local notifier = require("wpilib.notifier").create_notifier("Deploy Robot Code")
+	require("wpilib.commands.commands").run_gradlew("deploy" .. (args.args or ""), function(_, data)
+		if not data or data == "" then
+			return
 		end
-	)
+
+		notifier.update("Building Robot Code...", 50)
+
+		if data:find("BUILD SUCCESSFUL") then
+			notifier.update("Robot Code Built!", 75)
+		elseif data:find("BUILD FAILED") then
+			notifier.finish("Deploy Failed! Check logs.")
+		end
+	end, function(_)
+		notifier.finish("Robot Code Deployed!")
+	end)
 end
 
 return Command
